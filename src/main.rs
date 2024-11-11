1b8a14f55836fcd46a3d50b04afeddf158a2e08b
hong.jian.li <hong.jian.li@gdscn.ey.com>
2024-11-11 10:35:41 +0800

    refactor: 将两次按下和释放大写锁定键的操作封装成一个函数 toggle_caps_lock，以减少重复代码

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,36 @@ use ctrlc;
 use inputbot::KeybdKey;
 use std::thread;
 use std::time::Duration;
+
+// 定义一个函数来模拟两次按下和释放大写锁定键
+fn toggle_caps_lock(key: &KeybdKey) {
+    key.press();
+    key.release();
+    key.press();
+    key.release();
+}
+
 fn main() {
     let caps_lock_key = KeybdKey::CapsLockKey;
 
     // 定义信号处理器
     let signal_handler = move || {
-        caps_lock_key.press();
-        caps_lock_key.release();
-        caps_lock_key.press();
-        caps_lock_key.release();
+        toggle_caps_lock(&caps_lock_key);
         std::process::exit(0);
     };
 
     // 注册信号处理器
     ctrlc::set_handler(signal_handler).expect("Error setting Ctrl-C handler");
+
     // 主循环
     loop {
+        // 打印当前时间
         println!("{:?}", Local::now());
-        caps_lock_key.press();
-        caps_lock_key.release();
-        caps_lock_key.press();
-        caps_lock_key.release();
+
+        // 模拟两次按下和释放大写锁定键
+        toggle_caps_lock(&caps_lock_key);
+
+        // 线程睡眠60秒
         thread::sleep(Duration::from_secs(60));
     }
 }
